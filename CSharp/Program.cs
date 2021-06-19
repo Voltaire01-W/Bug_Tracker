b7c9b282b7e301ba6ff54bb25e7ad593ea6f5a76
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-06-19 12:59:12 -0500

    Practicing CSharp

---
+++ CSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int totalNumber = 0;
+
+            int[] myArray = new int[] {
+                1, 2, 3, 4, 5
+            };
+
+            for (var x = 0; x < myArray.Length; x++) {
+                totalNumber = totalNumber + myArray[x];
+            }
+
+            Console.WriteLine(totalNumber);
+
+        }
+    }
+}
